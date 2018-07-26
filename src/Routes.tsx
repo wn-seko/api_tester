7024d06b086d817db771973de3b2ed5943cb5a5d
wn-seko <wataru_nakaseko@albert2005.co.jp>
2018-07-26 17:58:29 +0900

    Change path

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -7,11 +7,8 @@ import Top from './containers/pages/Top'
 const Routes = () => {
   return (
     <Switch>
-      <Route exact={true} path="/" render={() => <Redirect to="/top" />} />
-      <Switch>
-        <Route exact={true} path="/top" component={Top} />
-      </Switch>
-      <Route path="*" render={() => <Redirect to="/top" />} />
+      <Route exact={true} path="/" component={Top} />
+      <Route path="*" render={() => <Redirect to="/" />} />
     </Switch>
   )
 }
